43520238582d62d37589ab1c6653e2e9c1c5748d
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-02-17 20:00:29 -0500

    feat(monorepo): add and configure prettier (#2)
    Prettier is an opinionated code formatter. Any formatting done by Prettier should never affect the meaning of the code, so Prettier should be able to run automatically in the background. And since its opinionated, only one convention is needed for the monorepo. For these reasons, Prettier is configured globally at the monorepo root.

---
+++ prettier.config.js
@@ -0,0 +1,35 @@
+/* Documentation for this file: https://prettier.io/docs/en/configuration.html */
+
+/** @type {import("prettier").Config} */
+const config = {
+  // Extend the line length that the printer will wrap on beyond the default of 80.
+  printWidth: 100,
+
+  // Indent lines with spaces instead of tabs.
+  useTabs: false,
+
+  // Print spaces between brackets in object literals.
+  bracketSpacing: true,
+
+  // Use .gitattributes to manage newlines.
+  endOfLine: 'auto',
+
+  // Use single quotes instead of double quotes.
+  singleQuote: true,
+
+  // Trailing commas wherever possible (including function parameters and calls).
+  trailingComma: 'all',
+
+  // Only add semicolons at the beginning of lines that may introduce ASI failures.
+  semi: false,
+
+  // Enforce single attribute per line in HTML, Svelte, Vue and JSX.
+  singleAttributePerLine: false,
+
+  // Put the > of a multi-line HTML element at the end of the last line instead of being alone on the next line.
+  bracketSameLine: true,
+
+  plugins: ['prettier-plugin-packagejson'],
+}
+
+export default config

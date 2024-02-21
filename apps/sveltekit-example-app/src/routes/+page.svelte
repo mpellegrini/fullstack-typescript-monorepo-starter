dce4602fef3cf0330f31b6de67710125b52ff058
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-02-20 19:01:39 -0500

    feat(apps/sveltekit-example-app): use example-pkg (#13)
    Use the interface and utility function in example-pkg
    to build out a SvelteKit PageLoad for the default
    route.

--- apps/sveltekit-example-app/src/routes/+page.svelte
+++ apps/sveltekit-example-app/src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script lang="ts">
+  import { greetUser } from '@packages/example-pkg'
+
+  import type { PageData } from './$types'
+
+  export let data: PageData
+
+  $: greeting = greetUser(data.user)
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>{greeting}</p>

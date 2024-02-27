6b66f4b94946c9eb16a849694f946913e59ddb0c
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-02-27 15:59:25 -0500

    feat(toolchain/eslint-config): add more rule to eslint-plugin-vitest

--- packages/example-pkg/src/index.test.ts
+++ packages/example-pkg/src/index.test.ts
@@ -9,11 +9,15 @@ describe('greet user tests', () => {
   }
 
   it('user is an admin', () => {
+    expect.assertions(1)
+
     user.is_admin = true
     expect(greetUser(user)).toBe('Hello, Bob Loblaw! You are an admin.')
   })
 
   it('should greet user as a non-admin', () => {
+    expect.assertions(1)
+
     user.is_admin = false
     expect(greetUser(user)).toBe('Hello, Bob Loblaw! You are not an admin.')
   })

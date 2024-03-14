2ec83bd4de3344e6f21f49d5c9111eb46bf46e4a
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-03-14 14:10:33 -0400

    feat(apps/example-app): add initial tailwindcss config (#21)

--- apps/sveltekit-example-app/src/routes/+page.svelte
+++ apps/sveltekit-example-app/src/routes/+page.svelte
@@ -8,6 +8,11 @@
   $: greeting = greetUser(data.user)
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>{greeting}</p>
+<div class="space-y-1">
+  <h1 class="text-4xl">Welcome to SvelteKit</h1>
+  <p>
+    Visit <a class="text-blue-600 hover:underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to
+    read the documentation
+  </p>
+  <p class="text-2xl">{greeting}</p>
+</div>

647942270e1984d49c856b9bfde7cd380bce7d64
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-03-26 18:49:35 -0400

    fix(monorepo): docker-compose.yml init script issue with create extension (#34)

--- .docker/initdb-extensions.sh
+++ .docker/initdb-extensions.sh
@@ -5,7 +5,6 @@ set -e
 echo "Loading extensions into $DB"
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-# must quote extension names or else symbolic error will be thrown.
-CREATE EXTENSION IF NOT EXISTS 'citext';
+CREATE EXTENSION IF NOT EXISTS citext;
 select * FROM pg_extension;
 EOSQL

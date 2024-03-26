e3fa96dbce2e1a87267ee14e3fc79a9d08b3341e
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-03-26 14:18:50 -0400

    feat(packages/lucia-auth): add lucia auth client

--- packages/drizzledb-pg/src/schema/index.ts
+++ packages/drizzledb-pg/src/schema/index.ts
@@ -1,1 +1,24 @@
+import { pgSchema, text, timestamp, uuid } from 'drizzle-orm/pg-core'
+
+import { citext } from './custom-types.js'
+
 export { count } from 'drizzle-orm'
+
+export const authSchema = pgSchema('auth')
+
+export const users = authSchema.table('users', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  username: citext('user_name').notNull(),
+  hashedPassword: text('hashed_password').notNull(),
+})
+
+export const sessions = authSchema.table('sessions', {
+  id: text('id').primaryKey(),
+  userId: uuid('user_id')
+    .notNull()
+    .references(() => users.id),
+  expiresAt: timestamp('expires_at', {
+    withTimezone: true,
+    mode: 'date',
+  }).notNull(),
+})

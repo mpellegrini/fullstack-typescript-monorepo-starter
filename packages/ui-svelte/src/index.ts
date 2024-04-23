2d1680834f126eb3763861e36a4ddf6bf4e08b2d
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-04-23 10:26:56 -0400

    feat(packages/ui-svelte): add input component
    Added via shadcn-svelte and modified to fit this
    library.

---
+++ packages/ui-svelte/src/index.ts
@@ -0,0 +1,44 @@
+/**
+ * src/components/atoms/accordion
+ */
+export {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from './components/atoms/accordion/index.js'
+
+/**
+ * src/components/atoms/button
+ */
+export {
+  Button,
+  type ButtonEvents,
+  type ButtonProps,
+  buttonVariants,
+} from './components/atoms/button/index.js'
+
+/**
+ * src/components/atoms/form
+ */
+export {
+  FormField,
+  FormControl,
+  FormDescription,
+  FormLabel,
+  FormFieldErrors,
+  FormFieldset,
+  FormLegend,
+  FormElementField,
+  FormButton,
+} from './components/atoms/form/index.js'
+
+/**
+ * src/components/atoms/input
+ */
+export { Input } from './components/atoms/input/index.js'
+
+/**
+ * src/components/atoms/label
+ */
+export { Label } from './components/atoms/label/index.js'

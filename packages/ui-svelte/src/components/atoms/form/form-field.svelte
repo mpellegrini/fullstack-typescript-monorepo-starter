4458ec9ed47fa4085a710a2f1e1605c030139c51
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-04-22 22:50:24 -0400

    feat(packages/ui-svelte): add more components
    Added via shadcn-svelte and modified to fit this
    library:
    
    * form
    * label
    * accordion

---
+++ packages/ui-svelte/src/components/atoms/form/form-field.svelte
@@ -0,0 +1,24 @@
+<script lang="ts" generics="T extends Record<string, unknown>">
+  import * as FormPrimitive from 'formsnap'
+
+  import { cn } from '../../../utils/index.js'
+
+  import type { HTMLAttributes } from 'svelte/elements'
+  import type { FormPath, SuperForm } from 'sveltekit-superforms'
+
+  type U = FormPath<T>
+
+  type $$Props = FormPrimitive.FieldProps<T, U> & HTMLAttributes<HTMLElement>
+
+  export let form: SuperForm<T>
+  export let name: U
+
+  let className: $$Props['class'] = undefined
+  export { className as class }
+</script>
+
+<FormPrimitive.Field {form} {name} let:constraints let:errors let:tainted let:value>
+  <div class={cn('space-y-2', className)}>
+    <slot {constraints} {errors} {tainted} {value} />
+  </div>
+</FormPrimitive.Field>

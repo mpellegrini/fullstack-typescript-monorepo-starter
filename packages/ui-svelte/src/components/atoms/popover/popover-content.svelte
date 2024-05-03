5bc1893246365969d21e140de52fca35be6d4537
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-05-03 16:32:40 -0400

    feat(packages/ui-svelte): add more components (#62)
    Added via shadcn-svelte and modified to fit this
    library:
    
    * command
    * popover
    * tabs

---
+++ packages/ui-svelte/src/components/atoms/popover/popover-content.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Popover as PopoverPrimitive } from 'bits-ui'
+
+  import { type ClassValue, cn, flyAndScale } from '../../../utils/index.js'
+
+  type $$Props = PopoverPrimitive.ContentProps
+  let className: ClassValue = undefined
+  export let transition: $$Props['transition'] = flyAndScale
+  export let transitionConfig: $$Props['transitionConfig'] = undefined
+  export { className as class }
+</script>
+
+<PopoverPrimitive.Content
+  {transition}
+  {transitionConfig}
+  class={cn(
+    'bg-popover text-popover-foreground z-50 w-72 rounded-md border p-4 shadow-md outline-none',
+    className,
+  )}
+  {...$$restProps}>
+  <slot />
+</PopoverPrimitive.Content>
---
+++ packages/ui-svelte/src/components/atoms/popover/popover.story.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { Button } from '../button/index.js'
+  import { Input } from '../input/index.js'
+  import { Label } from '../label/index.js'
+
+  import { Popover, PopoverContent, PopoverTrigger } from './index.js'
+
+  import type { Hst as Histoire } from '@histoire/plugin-svelte'
+
+  export let Hst: Histoire
+</script>
+
+<Hst.Story title="Atoms/Popover">
+  <Popover portal="null">
+    <PopoverTrigger asChild let:builder>
+      <Button builders={[builder]} variant="outline">Open</Button>
+    </PopoverTrigger>
+    <PopoverContent class="w-80">
+      <div class="grid gap-4">
+        <div class="space-y-2">
+          <h4 class="font-medium leading-none">Dimensions</h4>
+          <p class="text-muted-foreground text-sm">Set the dimensions for the layer.</p>
+        </div>
+        <div class="grid gap-2">
+          <div class="grid grid-cols-3 items-center gap-4">
+            <Label for="width">Width</Label>
+            <Input id="width" value="100%" class="col-span-2 h-8" />
+          </div>
+          <div class="grid grid-cols-3 items-center gap-4">
+            <Label for="height">Height</Label>
+            <Input id="height" value="25px" class="col-span-2 h-8" />
+          </div>
+        </div>
+      </div>
+    </PopoverContent>
+  </Popover>
+</Hst.Story>

5470440640319f6d7de9523d556e2b7e2414c456
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-07-01 15:22:55 -0400

    refactor(packages/ui-svelte): remove unused components (#78)
    Removing unused components ahead of adopting a new approach
    for a component library not based on shadcn-svelte

--- packages/ui-svelte/src/index.ts
+++ packages/ui-svelte/src/index.ts
@@ -1,61 +1,9 @@
 export {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from './components/atoms/accordion/index.js'
-
-export { Alert, AlertDescription, AlertTitle } from './components/atoms/alert/index.js'
-
-export {
-  AlertDialog,
-  AlertDialogTitle,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogPortal,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTrigger,
-  AlertDialogOverlay,
-  AlertDialogContent,
-  AlertDialogDescription,
-} from './components/atoms/alert-dialog/index.js'
-
-export { Avatar, AvatarImage, AvatarFallback } from './components/atoms/avatar/index.js'
-
-export { Badge, badgeVariants } from './components/atoms/badge/index.js'
-
-export {
   Button,
   type ButtonEvents,
   type ButtonProps,
   buttonVariants,
 } from './components/atoms/button/index.js'
-
-export {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-  type HeadingLevel,
-} from './components/atoms/card/index.js'
-
-export { Checkbox } from './components/atoms/checkbox/index.js'
-
-export {
-  Dialog,
-  DialogTitle,
-  DialogPortal,
-  DialogFooter,
-  DialogHeader,
-  DialogTrigger,
-  DialogOverlay,
-  DialogContent,
-  DialogDescription,
-  DialogClose,
-} from './components/atoms/dialog/index.js'
 
 export {
   FormField,
@@ -72,5 +20,3 @@ export {
 export { Input } from './components/atoms/input/index.js'
 
 export { Label } from './components/atoms/label/index.js'
-
-export { Switch } from './components/atoms/switch/index.js'

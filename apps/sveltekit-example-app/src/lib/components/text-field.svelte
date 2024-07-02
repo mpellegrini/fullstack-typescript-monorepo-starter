c7ce18eb49e104586e648f5f34f931f28b49c75c
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-07-02 19:52:50 -0400

    feat(apps/sveltekit-example-app): add more features of superforms to sign-up page
    fix typecheck issues

---
+++ apps/sveltekit-example-app/src/lib/components/text-field.svelte
@@ -0,0 +1,20 @@
+<script lang="ts" generics="T extends Record<string, unknown>">
+  import { type FormPathLeaves, type SuperForm, formFieldProxy } from 'sveltekit-superforms'
+
+  export let form: SuperForm<T>
+  export let field: FormPathLeaves<T>
+
+  const { value, errors, constraints } = formFieldProxy(form, field)
+</script>
+
+<label>
+  {field}<br />
+  <input
+    name={field}
+    type="email"
+    aria-invalid={$errors ? 'true' : undefined}
+    bind:value={$value}
+    {...$constraints}
+    {...$$restProps} />
+</label>
+{#if $errors}<span class="invalid">{$errors}</span>{/if}

ad75858feefd4540ae10348b01ec89ed3e3fd595
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-07-04 12:52:25 -0400

    feat(packages/ui-lib-svelte): initial ui component library for svelte (#84)
    This replaces packages/ui-svelte package. That package was
    a standalone svelte app. This library is a SvelteKit app and
    will allow for creating a more integrated component testing
    app using something like KitBook or StoryBook.

--- packages/ui-svelte/src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-import { type ClassValue as ClsxClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export type ClassValue = string | null | undefined
-
-export const cn = (...inputs: ClsxClassValue[]): string => {
-  return twMerge(clsx(inputs))
-}

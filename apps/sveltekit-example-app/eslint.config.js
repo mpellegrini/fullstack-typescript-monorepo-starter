b7e4063146cb5d1f9afa2d97a49077faeee2cf87
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-08-09 19:06:52 -0400

    feat(apps/sveltekit-example-app): svelte5 replace slot with render

--- apps/sveltekit-example-app/eslint.config.js
+++ apps/sveltekit-example-app/eslint.config.js
@@ -10,6 +10,7 @@ export default [
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
     },
   },

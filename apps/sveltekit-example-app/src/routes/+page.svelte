39670d2149baf17fe5a95d7fe43e10818665e096
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-08-14 08:08:46 -0400

    refactor(apps/sveltekit-example-app): remove example-pkg dep
    @packages/example-pkg was being used to test
    out functionality with eslint configuration.
    But that issue was solved awhile ago, so
    removing from the app as it is now getting
    in the way of adding new features.

--- apps/sveltekit-example-app/src/routes/+page.svelte
+++ apps/sveltekit-example-app/src/routes/+page.svelte
@@ -1,16 +1,7 @@
-<script lang="ts">
-  import { greetUser } from '@packages/example-pkg'
-
-  const { data } = $props()
-
-  const greeting = $derived(greetUser(data.user))
-</script>
-
 <div class="space-y-1">
   <h1 class="text-4xl">Welcome to SvelteKit</h1>
   <p>
     Visit <a class="text-blue-600 hover:underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to
     read the documentation
   </p>
-  <p class="text-2xl">{greeting}</p>
 </div>

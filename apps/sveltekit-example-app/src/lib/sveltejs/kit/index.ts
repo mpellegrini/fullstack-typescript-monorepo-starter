f7ec0fd62351a24330597bb88d5b6962abdae15b
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-08-18 09:46:47 -0400

    feat(packages/api): move user sign-up under api
    Not using this code anymore

--- apps/sveltekit-example-app/src/lib/sveltejs/kit/index.ts
+++
@@ -1,7 +0,0 @@
-import type { MaybePromise } from '$app/navigation'
-import type { RequestEvent, ResolveOptions } from '@sveltejs/kit'
-
-export type Handle = (input: {
-  event: RequestEvent
-  resolve: (event: RequestEvent, opts?: ResolveOptions) => MaybePromise<Response>
-}) => MaybePromise<Response>

98015625012f5b5d23b79b0ec2b1249488618d51
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-08-28 17:31:58 -0400

    feat(packages/db-drizzlepg): logging enhancements
    * pretty format sql
    * parameter substitution in outputted sql

--- packages/db-drizzlepg/src/client/utils.ts
+++ packages/db-drizzlepg/src/client/utils.ts
@@ -1,10 +1,21 @@
 import type { Logger } from 'drizzle-orm'
 
+import { format } from 'sql-formatter'
+
 export class QueryLogger implements Logger {
   logQuery(query: string, params: unknown[]): void {
     console.debug('_____DRIZZLE_QUERY_____')
-    console.debug(query)
-    console.debug(params)
+    console.debug(
+      format(query, {
+        language: 'postgresql',
+        params: Object.fromEntries(
+          params.map((value, index) => [
+            index + 1,
+            typeof value === 'string' ? `'${value}'` : String(value),
+          ]),
+        ),
+      }),
+    )
     console.debug('___END_DRIZZLE_QUERY___')
   }
 }

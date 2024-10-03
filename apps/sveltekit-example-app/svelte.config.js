f1d0764013e3ab4e092df9872ebdecde7f071875
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-10-03 16:59:28 -0400

    fix(apps/sveltekit-example-app): add back vite preprocess (#172)
    It was initially thought that
    vitePreprocess was not needed with Svelte 5
    but its unclear if this ever was a true
    statement. Adding it back for now.

--- apps/sveltekit-example-app/svelte.config.js
+++ apps/sveltekit-example-app/svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-node'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,6 +15,9 @@ const config = {
       },
     },
   },
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 }
 
 export default config

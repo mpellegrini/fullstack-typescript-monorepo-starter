0ddbf5f4fbe846b9012b560c0a126852efc67aab
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-12-19 11:23:20 -0500

    feat(packages/ui-lib-svelte): add tailwindcss v4 preview and configure sveltekit
    fix missing import

--- packages/ui-lib-svelte/vite.config.js
+++ packages/ui-lib-svelte/vite.config.js
@@ -1,4 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import tailwindcss from '@tailwindcss/vite'
 import { createRequire } from 'node:module'
 import { defineConfig, mergeConfig } from 'vite'
 

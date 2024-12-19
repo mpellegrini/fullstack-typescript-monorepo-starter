0442924d932785d0c05ba73a9bc90d96c7a85712
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2024-12-19 11:24:56 -0500

    feat(packages/ui-lib-svelte): add tailwindcss v4 preview and configure sveltekit (#202)

--- packages/ui-lib-svelte/src/routes/+page.svelte
+++ packages/ui-lib-svelte/src/routes/+page.svelte
@@ -1,3 +1,8 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="space-y-1">
+  <h1 class="text-4xl">Welcome to your library project</h1>
+  <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+  <p>
+    Visit <a class="text-blue-600 hover:underline" href="https://svelte.dev/docs/kit"
+      >svelte.dev/docs/kit</a> to read the documentation
+  </p>
+</div>

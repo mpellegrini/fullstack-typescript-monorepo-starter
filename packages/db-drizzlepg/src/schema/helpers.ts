abae7bec72441ac255437345462cfe6f26c64eed
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-01-15 19:23:33 -0500

    fix(@packages/db-drizzlepg): ensure snake_case is used for constraint names

--- packages/db-drizzlepg/src/schema/helpers.ts
+++ packages/db-drizzlepg/src/schema/helpers.ts
@@ -1,4 +1,5 @@
 import { getTableName } from 'drizzle-orm'
+import { toSnakeCase } from 'drizzle-orm/casing'
 import {
   type ForeignKeyBuilder,
   type IndexBuilder,
@@ -12,22 +13,29 @@ import {
   unique,
 } from 'drizzle-orm/pg-core'
 
+/*
+  Note: In the below functions, when getting the column name, the non snake-case name is
+        returned (websiteUrl vs website_url). There is an open issue on this, so workaround
+        for now is to use the toSnakeCase function that Drizzle uses.
+        see: https://github.com/drizzle-team/drizzle-orm/issues/3094
+ */
+
 export const namedForeignKey = (column: PgColumn, foreignColumn: PgColumn): ForeignKeyBuilder =>
   foreignKey({
     columns: [column],
     foreignColumns: [foreignColumn],
-    name: `${getTableName(column.table)}_fk_${column.name}_${getTableName(foreignColumn.table)}`,
+    name: `${getTableName(column.table)}_fk_${toSnakeCase(column.name)}_${getTableName(foreignColumn.table)}`,
   })
 
 export const namedUnique = (...columns: [PgColumn, ...PgColumn[]]): UniqueConstraintBuilder => {
   const tableName = getTableName(columns[0].table)
-  const name = `${tableName}_uk_${columns.map((col) => col.name).join('_')}`
+  const name = `${tableName}_uk_${columns.map((col) => toSnakeCase(col.name)).join('_')}`
   return unique(name).on(...columns)
 }
 
 export const namedIndex = (...columns: [PgColumn, ...PgColumn[]]): IndexBuilder => {
   const tableName = getTableName(columns[0].table)
-  const name = `${tableName}_idx_${columns.map((col) => col.name).join('_')}`
+  const name = `${tableName}_idx_${columns.map((col) => toSnakeCase(col.name)).join('_')}`
   return index(name).on(...columns)
 }
 

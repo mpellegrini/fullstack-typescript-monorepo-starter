35264febfd14b4f755a3da7a7588c17edf613f59
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-02-02 13:53:12 -0500

    feat(monorepo): blah

--- apps/sveltekit-example-app/vite.config.js
+++ apps/sveltekit-example-app/vite.config.js
@@ -1,6 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import tailwindcss from '@tailwindcss/vite'
 import { createRequire } from 'node:module'
+import { bundleStats } from 'rollup-plugin-bundle-stats'
+import { visualizer } from 'rollup-plugin-visualizer'
+import webpackStatsPlugin from 'rollup-plugin-webpack-stats'
 import { defineConfig, mergeConfig } from 'vite'
 
 import vitestSharedConfig from '@toolchain/vitest-config'
@@ -11,7 +14,16 @@ const packageJson = require('./package.json')
 export default mergeConfig(
   vitestSharedConfig,
   defineConfig({
-    plugins: [sveltekit(), tailwindcss()],
+    plugins: [
+      sveltekit(),
+      tailwindcss(),
+      // bundleStats({ json: true }),
+      // visualizer({
+      //   emitFile: true,
+      //   filename: 'stats.html',
+      // }),
+      webpackStatsPlugin(),
+    ],
     test: {
       name: packageJson.name,
     },

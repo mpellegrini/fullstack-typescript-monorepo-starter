918d390e8b492d17c313a64c55224e4773d0d9bd
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-03-23 08:24:05 -0400

    refactor(packages/api-hono): remove swagger-ui and favicon middleware
    Looking to keep this base example as simple as possible.

--- packages/api-hono/src/lib/create-app.ts
+++ packages/api-hono/src/lib/create-app.ts
@@ -3,14 +3,13 @@ import { logger } from 'hono/logger'
 
 import type { AppOpenApi, CustomEnv } from './types.js'
 
-import { notFound, onError, serveEmojiFavicon } from '../middlewares/index.js'
+import { notFound, onError } from '../middlewares/index.js'
 
 export const createRouter = (): AppOpenApi => new OpenAPIHono<CustomEnv>({ strict: true })
 
 export const createApp = (): AppOpenApi => {
   const app = createRouter()
 
-  app.use(serveEmojiFavicon('🔥'))
   app.use(logger())
 
   app.notFound(notFound)
--- packages/api-hono/src/middlewares/index.ts
+++ packages/api-hono/src/middlewares/index.ts
@@ -1,3 +1,2 @@
 export { notFound } from './not-found.js'
 export { onError } from './on-error.js'
-export { serveEmojiFavicon } from './serve-emoji-favicon.js'

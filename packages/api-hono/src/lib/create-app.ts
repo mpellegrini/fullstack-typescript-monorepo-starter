a945e2d4529a509b7dca474e84f334752d6a661a
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-04-07 10:34:32 -0400

    refactor(packages/api-hono): move middleware out of create-app.ts (#267)
    Moving setting middleware out of create-app
    so that this function can be made as a
    reusable utility function.

--- packages/api-hono/src/lib/create-app.ts
+++ packages/api-hono/src/lib/create-app.ts
@@ -1,5 +1,4 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
-import { logger } from 'hono/logger'
 
 import type { AppOpenApi, CustomEnv } from './types.js'
 
@@ -7,15 +6,16 @@ import { defaultHook } from '../middlewares/default-hook.js'
 import { notFound, onError } from '../middlewares/index.js'
 
 export const createRouter = (): AppOpenApi =>
-  new OpenAPIHono<CustomEnv>({ defaultHook, strict: true })
+  new OpenAPIHono<CustomEnv>({
+    defaultHook,
+    strict: true,
+  })
 
-export const createApp = (basePath = '/'): AppOpenApi => {
-  const app = createRouter().basePath(basePath)
+export const createApp = (): AppOpenApi => {
+  const app = createRouter()
 
   app.notFound(notFound)
   app.onError(onError)
-
-  app.use('*', logger())
 
   return app
 }

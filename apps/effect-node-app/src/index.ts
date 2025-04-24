0b647a1d9ec5b2e3f6826c11970ad62d57765cbc
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-04-23 22:06:06 -0400

    feat(apps/effect-node-app): initial work on creating api node application (#284)

---
+++ apps/effect-node-app/src/index.ts
@@ -0,0 +1,16 @@
+import { HttpApiBuilder, HttpServer } from '@effect/platform'
+import { NodeHttpServer, NodeRuntime } from '@effect/platform-node'
+import { Layer } from 'effect'
+import { createServer } from 'node:http'
+
+import { openApiDocLayer, platformApiLive } from '@packages/api-effect/server'
+
+const HttpLive = HttpApiBuilder.serve().pipe(
+  Layer.provide(platformApiLive),
+  Layer.provide(openApiDocLayer),
+  HttpServer.withLogAddress,
+)
+
+NodeRuntime.runMain(
+  Layer.launch(HttpLive.pipe(Layer.provide(NodeHttpServer.layer(createServer, { port: 3000 })))),
+)

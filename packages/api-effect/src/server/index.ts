80dd9f2bf41ccc5298f34aa7c75f92a6523ad01d
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-04-23 22:02:31 -0400

    feat(apps/effect-node-app): initial work on creating api node application

--- packages/api-effect/src/server/index.ts
+++ packages/api-effect/src/server/index.ts
@@ -1,28 +1,18 @@
-import { HttpApiBuilder, HttpApiScalar, HttpServer } from '@effect/platform'
-import { NodeHttpServer, NodeRuntime } from '@effect/platform-node'
+import { HttpApiBuilder, HttpApiScalar } from '@effect/platform'
 import { Layer } from 'effect'
-import { createServer } from 'node:http'
 
 import { taskGroupLive } from '../impl/index.js'
 import { PlatformApi } from '../platform-api.js'
 
-const mainApiLive = HttpApiBuilder.api(PlatformApi).pipe(Layer.provide([taskGroupLive]))
+export const platformApiLive = HttpApiBuilder.api(PlatformApi).pipe(Layer.provide(taskGroupLive))
 
-const HttpLive = HttpApiBuilder.serve().pipe(
-  Layer.provide(HttpApiScalar.layer({ scalar: { layout: 'modern', theme: 'kepler' } })),
-  Layer.provide(mainApiLive),
-  HttpServer.withLogAddress,
+export const openApiDocLayer = HttpApiScalar.layer({
+  path: '/api/docs',
+  scalar: { layout: 'modern', theme: 'kepler' },
+}).pipe(Layer.provide(platformApiLive))
+
+/*
+export const webHandler = HttpApiBuilder.toWebHandler(
+  Layer.mergeAll(platformApiLive, openApiDocLayer, HttpServer.layerContext),
 )
-
-NodeRuntime.runMain(
-  Layer.launch(HttpLive.pipe(Layer.provide(NodeHttpServer.layer(createServer, { port: 3000 })))),
-)
-
-// const docLayer = HttpApiScalar.layer({
-//   path: '/api/docs',
-//   scalar: { layout: 'modern', theme: 'kepler' },
-// }).pipe(Layer.provide(mainApiLive))
-//
-// export const webHandler = HttpApiBuilder.toWebHandler(
-//   Layer.mergeAll(mainApiLive, docLayer, HttpServer.layerContext),
-// )
+*/

657004ea71c16c26fbe228232f73fc1b02d6b7d4
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-05-04 15:50:15 -0400

    feat(apps/sveltekit-example-app): add mode-watcher to layout (#300)

--- apps/sveltekit-example-app/src/routes/+layout.svelte
+++ apps/sveltekit-example-app/src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
   import '../app.css'
+
+  import { ModeWatcher } from 'mode-watcher'
+
   import type { LayoutProps } from './$types.js'
 
   const { children }: LayoutProps = $props()
 </script>
 
+<ModeWatcher />
 {@render children()}

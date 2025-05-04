1abbf592addf04023152707d054978b61ea3fea5
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-05-04 15:58:59 -0400

    fix(apps/sveltekit-example-app): remove leftover code in app.d.ts (#301)

--- apps/sveltekit-example-app/src/app.d.ts
+++ apps/sveltekit-example-app/src/app.d.ts
@@ -1,15 +1,10 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 
-import type { ApiClient } from '@packages/api/client'
-
 declare global {
   namespace App {
     // interface Error {}
-    interface Locals {
-      api: ApiClient['api']
-      getAuthedUser: () => Promise<string | null>
-    }
+    // interface Locals {}
     // interface PageData {}
     // interface PageState {}
     // interface Platform {}
--- apps/sveltekit-example-app/src/routes/+page.server.ts
+++ apps/sveltekit-example-app/src/routes/+page.server.ts
@@ -1,6 +1,3 @@
 import type { PageServerLoad } from './$types.js'
 
-export const load = (async ({ locals }) => {
-  const authedUser = await locals.getAuthedUser()
-  return { authedUser }
-}) satisfies PageServerLoad
+export const load = (() => ({})) satisfies PageServerLoad

5753e95adf98815d78c5b35e309d551765ba2337
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-06-09 16:01:24 -0400

    refactor(apps/effect-api-server): re-locate database live layer to api-impl package (#326)

--- packages/api-impl/src/index.ts
+++ packages/api-impl/src/index.ts
@@ -2,10 +2,16 @@ import { HttpApiBuilder } from '@effect/platform'
 import { Layer } from 'effect'
 
 import { Api } from '@packages/api'
+import { Database } from '@packages/db-drizzlepg/effect-client'
 
+import { DatabaseLive } from './services/index.js'
 import { taskGroupLive } from './tasks-live.js'
 
 export const ApiLive = HttpApiBuilder
   //
   .api(Api)
-  .pipe(Layer.provide(taskGroupLive))
+
+  .pipe(
+    Layer.merge(Layer.effectDiscard(Database.Database.use((db) => db.setupConnectionListeners))),
+    Layer.provide([taskGroupLive, DatabaseLive]),
+  )

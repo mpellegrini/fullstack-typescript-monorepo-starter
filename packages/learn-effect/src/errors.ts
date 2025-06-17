4061c06d1d9fad80743939b76e3997cfc5d36bef
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-06-17 11:00:30 -0400

    feat(packages/learn-effect): remove package as no longer required

--- packages/learn-effect/src/errors.ts
+++
@@ -1,5 +0,0 @@
-import { Data } from 'effect'
-
-export class FetchError extends Data.TaggedError('FetchError') {}
-
-export class JsonError extends Data.TaggedError('JsonError') {}
--- packages/learn-effect/src/schemas.ts
+++
@@ -1,9 +0,0 @@
-import { Schema } from 'effect'
-
-export class Pokemon extends Schema.Class<Pokemon>('Pokemon')({
-  id: Schema.Number,
-  height: Schema.Number,
-  name: Schema.String,
-  order: Schema.Number,
-  weight: Schema.Number,
-}) {}

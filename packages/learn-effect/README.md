87f125b71ffcaa91bec8d075eb588c42b53f74ea
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-06-17 12:30:42 -0400

    feat(packages/learn-effect): remove package as no longer required (#336)

--- packages/learn-effect/README.md
+++
@@ -1,5 +0,0 @@
-# Learn Effect
-
-## Effect: Beginners Complete Getting Started
-
-https://www.typeonce.dev/course/effect-beginners-complete-getting-started
--- packages/learn-effect/src/services/pokemon-collection.ts
+++
@@ -1,5 +0,0 @@
-import { Effect } from 'effect'
-
-export class PokemonCollection extends Effect.Service<PokemonCollection>()('PokemonCollection', {
-  succeed: ['staryu', 'perrserker', 'flaffy'],
-}) {}

df0077c4f9c6c5efc9565ffe235f20d20fdc5669
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-06-17 10:57:03 -0400

    refactor(packages/api-impl): use different style of providing config defaults (#335)

--- packages/api-impl/src/services/env-vars.ts
+++ packages/api-impl/src/services/env-vars.ts
@@ -4,21 +4,19 @@ export class EnvVars extends Effect.Service<EnvVars>()('EnvVars', {
   accessors: true,
   effect: Effect.gen(function* () {
     return {
-      CI: yield* Config.boolean('CI').pipe(Config.withDefault(false)),
+      CI: yield* Config.withDefault(Config.boolean('CI'), false),
       DB_CONNECTION_STRING: yield* Config.redacted('DB_CONNECTION_STRING'),
-      DB_IDLE_TIMEOUT_MILLIS: yield* Config.integer('DB_IDLE_TIMEOUT_MILLIS').pipe(
-        Config.withDefault(10_000),
+      DB_IDLE_TIMEOUT_MILLIS: yield* Config.withDefault(
+        Config.integer('DB_IDLE_TIMEOUT_MILLIS'),
+        10_000,
       ),
-      DB_LOGGING_ENABLED: yield* Config.boolean('DB_LOGGING_ENABLED').pipe(
-        Config.withDefault(false),
-      ),
+      DB_LOGGING_ENABLED: yield* Config.withDefault(Config.boolean('DB_LOGGING_ENABLED'), false),
       DB_MAX_CONNECTIONS: yield* Config.integer('DB_MAX_CONNECTIONS').pipe(Config.withDefault(10)),
-      ENV: yield* Config.literal(
-        'production',
+      ENV: yield* Config.withDefault(
+        Config.literal('production', 'development', 'test')('ENV'),
         'development',
-        'test',
-      )('ENV').pipe(Config.withDefault('development')),
-      PORT: yield* Config.integer().pipe(Config.withDefault(3000)),
+      ),
+      PORT: yield* Config.withDefault(Config.integer(), 3000),
     } as const
   }),
 }) {}

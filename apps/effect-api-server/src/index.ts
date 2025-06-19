336f06d0bf4ca63d141a0de3c2bddb73faa24882
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-06-19 17:48:59 -0400

    feat(packages/db-drizzlepg): remove effectful drizzle client
    Found an alternate approach, so blowing this one up

--- apps/effect-api-server/src/index.ts
+++ apps/effect-api-server/src/index.ts
@@ -1,26 +1,10 @@
 import { NodeRuntime } from '@effect/platform-node'
-import { Duration, Effect, Layer, Schedule } from 'effect'
+import { Layer } from 'effect'
 
 import { HttpLive } from './http.js'
 
 HttpLive.pipe(
+  //
   Layer.launch,
-  Effect.tapErrorCause(Effect.logError),
-  Effect.retry({
-    schedule: Schedule.exponential('1 second', 2).pipe(
-      Schedule.modifyDelay(Duration.min('8 seconds')),
-      Schedule.jittered,
-      Schedule.repetitions,
-      Schedule.modifyDelayEffect((count, delay) =>
-        Effect.as(
-          Effect.logError(
-            `[Server crashed]: Retrying in ${Duration.format(delay)} (attempt #${count + 1})`,
-          ),
-          delay,
-        ),
-      ),
-    ),
-    while: (error) => error._tag === 'DatabaseConnectionLostError',
-  }),
-  NodeRuntime.runMain({ disableErrorReporting: true }),
+  NodeRuntime.runMain({}),
 )

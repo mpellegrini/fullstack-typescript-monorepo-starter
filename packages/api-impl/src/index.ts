2ee5d7474dd47780889940e790a42526946725ed
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-06-19 17:45:51 -0400

    feat(packages/db-drizzlepg): remove effectful drizzle client
    Found an alternate approach, so blowing this one up

--- packages/api-impl/src/index.ts
+++ packages/api-impl/src/index.ts
@@ -2,17 +2,10 @@ import { HttpApiBuilder } from '@effect/platform'
 import { Layer } from 'effect'
 
 import { Api } from '@packages/api'
-import { Database } from '@packages/db-drizzlepg/effect-client'
 
-import { DatabaseLive } from './services/index.js'
 import { taskGroupLive } from './tasks-live.js'
 
 export const ApiLive = HttpApiBuilder
   //
   .api(Api)
-
-  .pipe(
-    Layer.merge(Layer.effectDiscard(Database.Database.use((db) => db.setupConnectionListeners))),
-    Layer.provide(DatabaseLive),
-    Layer.provide(taskGroupLive),
-  )
+  .pipe(Layer.provide(taskGroupLive))

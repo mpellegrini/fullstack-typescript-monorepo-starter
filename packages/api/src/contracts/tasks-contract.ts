b99a002da662ed7bf8b0df9244af847c89bc27cd
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-06-27 16:20:17 -0400

    feat(packages/api): add single item wrapper helper for structured response messages (#348)
    APIs that return a single item are wrapped in
    a standard message structure to allow for
    additional metadata about the response both now
    and in the future.

--- packages/api/src/contracts/tasks-contract.ts
+++ packages/api/src/contracts/tasks-contract.ts
@@ -9,20 +9,15 @@ import { Schema } from 'effect'
 import status from 'http-status'
 
 import * as CustomHttpApiError from '../custom-httpapi-error.js'
-import { TodoId } from '../entity-ids.js'
-
-export class Task extends Schema.Class<Task>('Task')({
-  id: TodoId,
-  done: Schema.Boolean,
-  name: Schema.NonEmptyTrimmedString,
-}) {}
+import { Task } from '../domain.js'
+import { singleItemResponseWrapperSchema } from '../response-helpers.js'
 
 export const UuidParamSchema = HttpApiSchema.param('id', Schema.UUID)
 
 export class ApiGroup extends HttpApiGroup.make('tasks') //
   .add(
     HttpApiEndpoint.get('getTaskById')`/${UuidParamSchema}`
-      .addSuccess(Task, { status: status.OK })
+      .addSuccess(singleItemResponseWrapperSchema(Task), { status: status.OK })
       .addError(CustomHttpApiError.BadRequest)
       .addError(HttpApiError.HttpApiDecodeError)
       .addError(CustomHttpApiError.NotFound),

f4349894aa4afa086d6b7ebdefe22f212c688f95
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-07-25 08:38:54 -0400

    feat(packages/api-impl): refactor task handler response and adjust eslint config
    removed uneeeded generic

--- packages/api-impl/src/tasks-live.ts
+++ packages/api-impl/src/tasks-live.ts
@@ -1,7 +1,7 @@
 import { HttpApiBuilder } from '@effect/platform'
 import { Effect } from 'effect'
 
-import { type Task, Api, TaskId, wrapSingleItemResponse } from '@packages/api'
+import { Api, TaskId, wrapSingleItemResponse } from '@packages/api'
 
 export const taskGroupLive = HttpApiBuilder.group(Api, 'tasks', (handlers) =>
   handlers.handle('getTaskById', ({ path }) =>
@@ -9,6 +9,6 @@ export const taskGroupLive = HttpApiBuilder.group(Api, 'tasks', (handlers) =>
       id: TaskId.make(path.id),
       done: false,
       name: 'My Found Task',
-    }).pipe(Effect.map(wrapSingleItemResponse<Task>)),
+    }).pipe(Effect.map(wrapSingleItemResponse)),
   ),
 )

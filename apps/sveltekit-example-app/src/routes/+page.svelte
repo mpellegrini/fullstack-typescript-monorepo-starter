71dc5ef61bd0334d6b7c7dfcd142c167ac63a97b
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-07-28 13:17:06 -0400

    feat(apps/sveltekit-example-app): add and configure ui-lib-svelte (#376)

--- apps/sveltekit-example-app/src/routes/+page.svelte
+++ apps/sveltekit-example-app/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { Button } from '@packages/ui-lib-svelte/components/ui/button'
 </script>
 
 <div class="space-y-1">
@@ -7,4 +8,5 @@
     Visit <a class="text-blue-600 hover:underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to
     read the documentation
   </p>
+  <Button variant="destructive">Click Me!</Button>
 </div>

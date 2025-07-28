8f42e36a5e5451963dff33cbe1bdba0a1e2bf7b0
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-07-28 11:22:59 -0400

    refactor(packages/ui-lib-svelte): change how tsconfig.json is extended (#373)
    Previously was using an approach provided
    via svelte.config.js. But this was always
    a bit non-standard. TypeScript has had
    support for extending multiple tsconfig.json
    files for a while now. This change favors
    the latter approach.

--- packages/ui-lib-svelte/svelte.config.js
+++ packages/ui-lib-svelte/svelte.config.js
@@ -8,12 +8,6 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
-    typescript: {
-      config: (config) => {
-        config['extends'] = '@toolchain/typescript-config/tsconfig-svelte.json'
-        return config
-      },
-    },
   },
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors

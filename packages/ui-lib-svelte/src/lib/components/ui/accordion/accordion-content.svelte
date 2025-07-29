7f693b30bf65667b3d9afebacd0759b09d6f6a70
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-07-29 19:59:29 -0400

    feat(packages/ui-lib-svelte): add accordion component (#378)

---
+++ packages/ui-lib-svelte/src/lib/components/ui/accordion/accordion-content.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { Accordion as AccordionPrimitive } from 'bits-ui'
+
+  import { type WithoutChild, cn } from '../../../utils.js'
+
+  let {
+    children,
+    class: className,
+    ref = $bindable(null),
+    ...restProps
+  }: WithoutChild<AccordionPrimitive.ContentProps> = $props()
+</script>
+
+<AccordionPrimitive.Content
+  class="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm"
+  data-slot="accordion-content"
+  bind:ref
+  {...restProps}>
+  <div class={cn('pb-4 pt-0', className)}>
+    {@render children?.()}
+  </div>
+</AccordionPrimitive.Content>
---
+++ packages/ui-lib-svelte/src/lib/components/ui/accordion/accordion.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Accordion as AccordionPrimitive } from 'bits-ui'
+
+  let {
+    ref = $bindable(null),
+    value = $bindable(),
+    ...restProps
+  }: AccordionPrimitive.RootProps = $props()
+</script>
+
+<AccordionPrimitive.Root
+  data-slot="accordion"
+  bind:ref
+  bind:value={value as never}
+  {...restProps} />

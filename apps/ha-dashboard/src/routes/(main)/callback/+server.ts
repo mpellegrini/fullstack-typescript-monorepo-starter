103a9dd12c8eeaf02ac0aab1ec5bedd4e55b6ad4
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-07-31 12:10:10 -0400

    feat(apps/ha-dashboard): initialize new sveltekit app for home automation

--- apps/ha-dashboard/src/routes/(main)/callback/+server.ts
+++ apps/ha-dashboard/src/routes/(main)/callback/+server.ts
@@ -1,11 +1,11 @@
-import { HOMEASSISTANT_BASE_URI } from '$env/static/private'
+import { env } from '$env/dynamic/private'
 
 import type { RequestHandler } from './$types'
 
 export const GET: RequestHandler = async ({ fetch, url }) => {
   const code = url.searchParams.get('code')
 
-  const baseUrl = HOMEASSISTANT_BASE_URI
+  const baseUrl = env['HOMEASSISTANT_BASE_URI']
 
   const tokenUrl = new URL('/auth/token', baseUrl)
   const body = `grant_type=authorization_code&code=${code}&client_id=http%3A%2F%2Flocalhost%3A5173%2F`

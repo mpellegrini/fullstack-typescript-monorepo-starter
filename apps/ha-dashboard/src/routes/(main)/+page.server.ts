4a3e1af7f8d74940bcbd08a18e41516c670fcebf
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-07-31 12:12:26 -0400

    feat(apps/ha-dashboard): initialize new sveltekit app for home automation (#381)

---
+++ apps/ha-dashboard/src/routes/(main)/+page.server.ts
@@ -0,0 +1,16 @@
+import { env } from '$env/dynamic/private'
+
+import type { PageServerLoad } from './$types'
+
+const getSignInUrl = (request: Request): string => {
+  const loginUrl = new URL('/auth/authorize', env['HOMEASSISTANT_BASE_URI'])
+  loginUrl.searchParams.set('response_type', 'code')
+  loginUrl.searchParams.set('client_id', request.url)
+  loginUrl.searchParams.set('redirect_uri', `${request.url}callback`)
+  loginUrl.searchParams.set('state', `${request.url}`)
+  return loginUrl.toString()
+}
+
+export const load: PageServerLoad = ({ request }) => ({
+  signInUrl: getSignInUrl(request),
+})

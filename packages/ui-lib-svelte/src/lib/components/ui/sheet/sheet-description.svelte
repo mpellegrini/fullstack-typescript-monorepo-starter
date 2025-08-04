6c4d429eacc5db47fe96387a9dbc9f57a557bcc3
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-08-04 08:56:42 -0400

    feat(packages/ui-lib-svelte): add shadcn sheet component (#385)

---
+++ packages/ui-lib-svelte/src/lib/components/ui/sheet/sheet-description.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Dialog as SheetPrimitive } from 'bits-ui'
+
+  import { cn } from '../../../utils.js'
+
+  let {
+    class: className,
+    ref = $bindable(null),
+    ...restProps
+  }: SheetPrimitive.DescriptionProps = $props()
+</script>
+
+<SheetPrimitive.Description
+  class={cn('text-muted-foreground text-sm', className)}
+  data-slot="sheet-description"
+  bind:ref
+  {...restProps} />
---
+++ packages/ui-lib-svelte/src/lib/components/ui/sheet/sheet-footer.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { HTMLAttributes } from 'svelte/elements'
+
+  import { type WithElementRef, cn } from '../../../utils.js'
+
+  let {
+    children,
+    class: className,
+    ref = $bindable(null),
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props()
+</script>
+
+<div
+  bind:this={ref}
+  class={cn('mt-auto flex flex-col gap-2 p-4', className)}
+  data-slot="sheet-footer"
+  {...restProps}>
+  {@render children?.()}
+</div>

eec655a20137e74088ebf7c5d6f249ed47c7de85
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-08-05 12:57:08 -0400

    feat(packages/ui-lib-svelte): add shadcn input component (#387)

---
+++ packages/ui-lib-svelte/src/lib/components/ui/input/input.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import type { HTMLInputAttributes, HTMLInputTypeAttribute } from 'svelte/elements'
+
+  import { type WithElementRef, cn } from '../../../utils.js'
+
+  type InputType = Exclude<HTMLInputTypeAttribute, 'file'>
+
+  type Props = WithElementRef<
+    Omit<HTMLInputAttributes, 'type'> &
+      ({ files?: FileList; type: 'file' } | { files?: undefined; type?: InputType })
+  >
+
+  let {
+    class: className,
+    files = $bindable(),
+    ref = $bindable(null),
+    type,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- SHADCN
+    value = $bindable(),
+    ...restProps
+  }: Props = $props()
+</script>
+
+{#if type === 'file'}
+  <input
+    bind:this={ref}
+    class={cn(
+      'selection:bg-primary dark:bg-input/30 selection:text-primary-foreground border-input ring-offset-background placeholder:text-muted-foreground shadow-xs flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 pt-1.5 text-sm font-medium outline-none transition-[color,box-shadow] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+      'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
+      'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+      className,
+    )}
+    data-slot="input"
+    type="file"
+    bind:files
+    bind:value
+    {...restProps} />
+{:else}
+  <input
+    bind:this={ref}
+    class={cn(
+      'border-input bg-background selection:bg-primary dark:bg-input/30 selection:text-primary-foreground ring-offset-background placeholder:text-muted-foreground shadow-xs flex h-9 w-full min-w-0 rounded-md border px-3 py-1 text-base outline-none transition-[color,box-shadow] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+      'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
+      'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+      className,
+    )}
+    data-slot="input"
+    {type}
+    bind:value
+    {...restProps} />
+{/if}

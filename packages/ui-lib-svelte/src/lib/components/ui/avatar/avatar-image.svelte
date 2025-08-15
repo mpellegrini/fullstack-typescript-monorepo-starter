2e05202770f0d743db84d35913c31fde7c65598b
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-08-15 15:10:16 -0400

    feat(packages/ui-lib-svelte): add avatar component

---
+++ packages/ui-lib-svelte/src/lib/components/ui/avatar/avatar-image.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Avatar as AvatarPrimitive } from 'bits-ui'
+
+  import { cn } from '../../../utils.js'
+
+  let {
+    class: className,
+    ref = $bindable(null),
+    ...restProps
+  }: AvatarPrimitive.ImageProps = $props()
+</script>
+
+<AvatarPrimitive.Image
+  class={cn('aspect-square size-full', className)}
+  data-slot="avatar-image"
+  bind:ref
+  {...restProps} />
---
+++ packages/ui-lib-svelte/src/lib/components/ui/avatar/avatar.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { Avatar as AvatarPrimitive } from 'bits-ui'
+
+  import { cn } from '../../../utils.js'
+
+  let {
+    class: className,
+    loadingStatus = $bindable('loading'),
+    ref = $bindable(null),
+    ...restProps
+  }: AvatarPrimitive.RootProps = $props()
+</script>
+
+<AvatarPrimitive.Root
+  class={cn('relative flex size-8 shrink-0 overflow-hidden rounded-full', className)}
+  data-slot="avatar"
+  bind:ref
+  bind:loadingStatus
+  {...restProps} />

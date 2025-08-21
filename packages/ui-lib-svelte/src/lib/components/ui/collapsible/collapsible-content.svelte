60f1688ab8d6caddf66ff73c11cbff35afd82adc
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-08-20 20:41:36 -0700

    feat(packages/ui-lib-svelte): add collapsible component (#402)

---
+++ packages/ui-lib-svelte/src/lib/components/ui/collapsible/collapsible-content.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { Collapsible as CollapsiblePrimitive } from 'bits-ui'
+
+  let { ref = $bindable(null), ...restProps }: CollapsiblePrimitive.ContentProps = $props()
+</script>
+
+<CollapsiblePrimitive.Content data-slot="collapsible-content" bind:ref {...restProps} />
---
+++ packages/ui-lib-svelte/src/lib/components/ui/collapsible/collapsible.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Collapsible as CollapsiblePrimitive } from 'bits-ui'
+
+  let {
+    open = $bindable(false),
+    ref = $bindable(null),
+    ...restProps
+  }: CollapsiblePrimitive.RootProps = $props()
+</script>
+
+<CollapsiblePrimitive.Root data-slot="collapsible" bind:ref bind:open {...restProps} />

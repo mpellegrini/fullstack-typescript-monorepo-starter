f0f83fab43f252b68b3b70e3e9234c4bb0052759
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2025-12-08 07:56:47 -0500

    feat(apps/effect-platform-fundamentals): inital work

--- apps/effect-platform-fundamentals/src/api-live.ts
+++ apps/effect-platform-fundamentals/src/api-live.ts
@@ -4,7 +4,12 @@ import * as Layer from 'effect/Layer'
 import { api } from './api.js'
 import { greetingsApiLive } from './greetings/api-implementation.js'
 
-// Provide the implementation for the API
+/**
+ * Represents the live HTTP API routes for the application.
+ *
+ * Note: This differs from the HttpApiBuilder.api, which produces an Api implementation.
+ *       const MyApiLive = HttpApiBuilder.api(api).pipe(Layer.provide(greetingsApiLive))
+ */
 export const httpApiRoutesLive = HttpLayerRouter.addHttpApi(api, {
   openapiPath: '/openapi.json',
 }).pipe(Layer.provide(greetingsApiLive))
--- apps/effect-platform-fundamentals/src/index.ts
+++ apps/effect-platform-fundamentals/src/index.ts
@@ -1,19 +1,25 @@
 import * as NodeHttpServer from '@effect/platform-node/NodeHttpServer'
 import * as NodeRuntime from '@effect/platform-node/NodeRuntime'
+import * as HttpApiScalar from '@effect/platform/HttpApiScalar'
 import * as HttpLayerRouter from '@effect/platform/HttpLayerRouter'
 import * as Effect from 'effect/Effect'
 import * as Layer from 'effect/Layer'
 import { createServer } from 'node:http'
 
 import { httpApiRoutesLive } from './api-live.js'
+import { api } from './api.js'
 
-const allRoutes = Layer.mergeAll(httpApiRoutesLive)
+const docsRouteLive = HttpApiScalar.layerHttpLayerRouter({
+  api: api,
+  path: '/docs',
+})
+
+const allRoutes = Layer.mergeAll(httpApiRoutesLive, docsRouteLive)
 
 /**
  * Serves the provided application layer as an HTTP server.
  */
 export const httpLayer = HttpLayerRouter.serve(allRoutes).pipe(
-  // Layer.provide(HttpApiScalar.layer()),
   Layer.provide(NodeHttpServer.layer(createServer, { port: 3000 })),
 )
 

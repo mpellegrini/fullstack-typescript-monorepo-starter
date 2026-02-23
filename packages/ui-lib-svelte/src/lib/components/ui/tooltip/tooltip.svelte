325ca1b3c36fe19a8145da1976f27c17d26ca2be
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2026-02-22 20:49:38 -0500

    feat(packages/ui-lib-svelte): refactor tooltip components and update `bits-ui` (#458)
    The previous version of the tooltip
    component was causing svelte-check
    issues. Updated to the latest version
    of shadcn-svelte to fix.

---
+++ packages/ui-lib-svelte/src/lib/components/ui/tooltip/tooltip.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { Tooltip as TooltipPrimitive } from 'bits-ui'
+
+  let { ref = $bindable(null), ...restProps }: TooltipPrimitive.TriggerProps = $props()
+</script>
+
+<TooltipPrimitive.Trigger data-slot="tooltip-trigger" bind:ref {...restProps} />

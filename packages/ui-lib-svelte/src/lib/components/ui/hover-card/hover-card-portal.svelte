6469109c2a8ab5aceaa14b72274d48150a4ed1c3
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2026-03-03 08:52:04 -0500

    refactor(packages/ui-lib-svelte): latest shadcn-svelte hover-card component (#470)

---
+++ packages/ui-lib-svelte/src/lib/components/ui/hover-card/hover-card-portal.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { LinkPreview as HoverCardPrimitive } from 'bits-ui'
+
+  const { ...restProps }: HoverCardPrimitive.PortalProps = $props()
+</script>
+
+<HoverCardPrimitive.Portal {...restProps} />

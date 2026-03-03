193258a8da2dac60b5b43600c040808ad20d9997
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2026-03-03 08:49:36 -0500

    refactor(packages/ui-lib-svelte): latest shadcn-svelte hover-card component

--- packages/ui-lib-svelte/src/lib/components/ui/hover-card/hover-card-content.svelte
+++ packages/ui-lib-svelte/src/lib/components/ui/hover-card/hover-card-content.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+  import type { ComponentProps } from 'svelte'
+
   import { LinkPreview as HoverCardPrimitive } from 'bits-ui'
 
-  import { cn } from '../../../utils.js'
+  import { type WithoutChildrenOrChild, cn } from '../../../utils.js'
+
+  import HoverCardPortal from './hover-card-portal.svelte'
 
   let {
     align = 'center',
@@ -11,14 +15,14 @@
     sideOffset = 4,
     ...restProps
   }: HoverCardPrimitive.ContentProps & {
-    portalProps?: HoverCardPrimitive.PortalProps
+    portalProps?: WithoutChildrenOrChild<ComponentProps<typeof HoverCardPortal>>
   } = $props()
 </script>
 
-<HoverCardPrimitive.Portal {...portalProps}>
+<HoverCardPortal {...portalProps}>
   <HoverCardPrimitive.Content
     class={cn(
-      'bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 mt-3 w-64 rounded-md border p-4 shadow-md outline-hidden outline-none',
+      'bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-end-2 data-[side=right]:slide-in-from-start-2 data-[side=top]:slide-in-from-bottom-2 z-50 mt-3 w-64 rounded-md border p-4 shadow-md outline-hidden outline-none',
       className,
     )}
     {align}
@@ -26,4 +30,4 @@
     {sideOffset}
     bind:ref
     {...restProps} />
-</HoverCardPrimitive.Portal>
+</HoverCardPortal>

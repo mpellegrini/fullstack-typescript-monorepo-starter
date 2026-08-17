04a0bdb97a163ece394091112b3b50655187d227
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2026-08-17 14:06:01 -0400

    refactor(packages/db-drizzlepg): add @effect/sql-pg

--- packages/db-drizzlepg/src/client/drizzle-nodepg-client.ts
+++ packages/db-drizzlepg/src/client/drizzle-nodepg-client.ts
@@ -1,16 +1,14 @@
-import type pg from 'pg'
-
-import { type NodePgDatabase, drizzle } from 'drizzle-orm/node-postgres'
+import * as PgClient from '@effect/sql-pg/PgClient'
+import * as PgDrizzle from 'drizzle-orm/effect-postgres'
 import * as Config from 'effect/Config'
 import * as Context from 'effect/Context'
 import * as Effect from 'effect/Effect'
 import * as Layer from 'effect/Layer'
 
 import { toTaggedErrorOrThrow } from './database-errors.ts'
-import { createNodePgPool } from './nodepg-pool.ts'
-// https://pbs.twimg.com/media/HHL_-UVXgAApwBr?format=jpg&name=4096x4096
-type DrizzleClient = NodePgDatabase & {
-  $client: pg.Pool
+
+type DrizzleClient = PgDrizzle.EffectPgDatabase & {
+  $client: PgClient.PgClient
 }
 
 interface DrizzleNodePgClientImpl {
@@ -18,20 +16,21 @@ interface DrizzleNodePgClientImpl {
   readonly use: <T>(fn: (client: DrizzleClient) => Promise<T>) => Effect.Effect<T, Error>
 }
 
+const PgClientLive = PgClient.layerConfig({
+  applicationName: Config.succeed('node-postgres-pool'),
+  connectTimeout: Config.succeed('0 millis'),
+  idleTimeout: Config.succeed('10 seconds'),
+  maxConnections: Config.succeed(10),
+  minConnections: Config.succeed(0),
+  url: Config.redacted('DB_CONNECTION_STRING'),
+})
+
 export class DrizzleNodePgClient extends Context.Service<
   DrizzleNodePgClient,
   DrizzleNodePgClientImpl
 >()('packages/db-drizzlepg/DrizzleNodePgClient', {
   make: Effect.gen(function* () {
-    const { pool } = yield* createNodePgPool({
-      connectionString: yield* Config.redacted('DB_CONNECTION_STRING'),
-      // TODO: need to add additional connection config
-    })
-
-    const db = drizzle({
-      client: pool,
-      logger: false,
-    })
+    const db = yield* PgDrizzle.makeWithDefaults()
 
     const use = Effect.fn(<T>(fn: (client: DrizzleClient) => Promise<T>) =>
       Effect.tryPromise({
@@ -43,5 +42,5 @@ export class DrizzleNodePgClient extends Context.Service<
     return { db, use } satisfies DrizzleNodePgClientImpl
   }),
 }) {
-  static readonly layer = Layer.effect(this)(this.make)
+  static readonly layer = Layer.effect(this)(this.make).pipe(Layer.provide(PgClientLive))
 }

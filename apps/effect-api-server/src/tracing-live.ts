414432b14ca27267b40a4f14939141abcb86abee
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2026-08-31 13:28:50 -0400

    refactor(apps/effect-api-server): minor code cleanup (#656)

--- apps/effect-api-server/src/tracing-live.ts
+++ apps/effect-api-server/src/tracing-live.ts
@@ -1,25 +1,28 @@
 import { NodeSdk } from '@effect/opentelemetry'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base'
-import { Config, Effect, Layer } from 'effect'
+import { Config, Effect, Layer, Option } from 'effect'
 
-export const TracingLive = Layer.unwrap(
-  Effect.gen(function* () {
-    const endpoint = yield* Config.option(Config.url('OTEL_EXPORTER_OTLP_ENDPOINT'))
-    if (endpoint._tag === 'None') {
-      yield* Effect.logWarning(
-        'OpenTelemetry Exporter OTLP Endpoint not defined. OTEL Traces will not be exported.',
-      )
-      return Layer.empty
-    }
-    return NodeSdk.layer(() => ({
-      resource: {
-        serviceName: 'effect-api-server',
-        serviceVersion: 'v1',
-      },
-      spanProcessor: new BatchSpanProcessor(
-        new OTLPTraceExporter({ url: `${endpoint.value.origin}/v1/traces` }),
-      ),
-    }))
-  }),
+export const TracingLive = Config.option(Config.url('OTEL_EXPORTER_OTLP_ENDPOINT')).pipe(
+  Effect.flatMap(
+    Option.match({
+      onNone: () =>
+        Effect.logWarning(
+          'OpenTelemetry Exporter OTLP Endpoint not defined. OTEL Traces will not be exported.',
+        ).pipe(Effect.as(Layer.empty)),
+      onSome: (endpoint) =>
+        Effect.succeed(
+          NodeSdk.layer(() => ({
+            resource: {
+              serviceName: 'effect-api-server',
+              serviceVersion: 'v1',
+            },
+            spanProcessor: new BatchSpanProcessor(
+              new OTLPTraceExporter({ url: `${endpoint.origin}/v1/traces` }),
+            ),
+          })),
+        ),
+    }),
+  ),
+  Layer.unwrap,
 )

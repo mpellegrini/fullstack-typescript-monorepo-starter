2e74de28cc8d9d8a44fa855672057317bd4b431e
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2026-09-14 13:53:14 -0400

    chore(apps/effect-platform-fundamentals): remove obsolete project

--- apps/effect-platform-fundamentals/src/api.ts
+++
@@ -1,9 +0,0 @@
-import * as HttpApi from 'effect/unstable/httpapi/HttpApi'
-
-import { greetingsApi } from './greetings/api-definition.ts'
-
-/**
- * After defining your groups, you can combine them into one HttpApi representing
- * your entire set of endpoints.
- */
-export const api = HttpApi.make('api').add(greetingsApi)
--- apps/effect-platform-fundamentals/vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineNodeBuildConfig, mergeConfig } from '@toolchain/vite'
-import { defineTestConfig } from '@toolchain/vitest'
-
-import packageJson from './package.json' with { type: 'json' }
-
-export default mergeConfig(defineTestConfig(packageJson), defineNodeBuildConfig(packageJson))
